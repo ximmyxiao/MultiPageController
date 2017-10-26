103e70abbff682449bbb55cde1f824e95c67abe4
Piao Piao <94313353@qq.com>
2017-10-26 11:06:13 +0800

    Initial Commit

---
+++ MultiPageController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiPageController
+//
+//  Created by Piao Piao on 2017/10/26.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiPageController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiPageController
+//
+//  Created by Piao Piao on 2017/10/26.
+//  Copyright © 2017年 Piao Piao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
